a9b06436deb80ec33dbdc52978c0c96d16772939
Erik Töger <erik.toger@gmail.com>
2023-08-13 17:58:15 +0200

    My first app

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) => {
+
+    var reader = new StreamReader (context.Request.Body);
+    var body = await reader.ReadToEndAsync();
+    var obj = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+    if (obj.ContainsKey("firstname"))
+    {
+        await context.Response.WriteAsync(obj["firstname"]);
+    }
+    
+
+});
 
 app.Run();
