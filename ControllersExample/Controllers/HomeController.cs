da27952bc8b964f8d984dc6407555f793512ed3e
Erik Töger <erik.toger@gmail.com>
2023-08-21 20:47:46 +0200

    Controller

---
+++ ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("file")]
+        public VirtualFileResult File()
+        {
+            return new VirtualFileResult("/collision.png", "image/png");
+        }
+
+        [Route("bookstore")]
+        public IActionResult BookStore()
+        {
+            return new RedirectToActionResult("Books","Store", new {});
+        }
+
+        [Route("/books")]
+        public IActionResult Book()
+        {
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                Response.StatusCode = 400;
+                return BadRequest("Book Id is not supplied");
+            }
+
+            if (Request.Query["bookid"] == "404")
+            {
+                return new NotFoundResult();
+            }
+
+            return new VirtualFileResult("/collision.png", "image/png");
+        }
+
+        [Route("")]
+        public string Base()
+        {
+            return "Base";
+        }
+    }
+}
---
+++ ControllersExample/Controllers/StoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("store/books")]
+        public IActionResult Books()
+        {
+            return Ok("This is the new route for the book store");
+        }
+    }
+}
