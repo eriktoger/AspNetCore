3d5e36c620e68b327bae72e4ca70caebbb26e4fc
Erik Töger <erik.toger@gmail.com>
2024-03-21 12:59:24 +0100

    Modal bindings

---
+++ IActionResultExample/IActionResultExample/Models/Books.cs
@@ -0,0 +1,13 @@
+namespace IActionResultExample.Models
+{
+    public class Book
+    {
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book id: {BookId} - Author {Author}";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 My repo for https://www.udemy.com/course/asp-net-core-true-ultimate-guide-real-project
+
+
+## Get debugger to work in VS-code
+- Install C# Dev kit
+- Change launch.json to point to current
+- Go to debug and choose C# ... and select the name that you chose in launch.json
